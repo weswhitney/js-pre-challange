674b129eeec0eafb8a29bdec9cb4cae972f962f8
weswhitney <wesleywhitney@gmail.com>
2015-05-26 15:17:32 -0600

    added some comments, just using it as a sandbox for right now

--- spec/spec.js
+++ spec/spec.js
@@ -2,8 +2,9 @@ var yourCode = require('../src');
 
 describe("largestPhoneNumber", function() {
 
-  it("returns the numbers without the dashes", function() {
-    expect(yourCode.largestPhoneNumber('123-456-7777')).toEqual('1234567777');
+
+  it("", function () {
+    expect(yourCode.largestPhoneNumber('123-456-7777')).toEqual(49);
   });
 
 });
--- src.js
+++ src.js
@@ -1,8 +1,27 @@
 module.exports = {
 
-  largestPhoneNumber: function(number) {
-    var numberNoDash = number.replace(/[^0-9]/g,'');
-    return numberNoDash
-  },
+  largestPhoneNumber: function(numbers) {
+    var total = 0, count = 1;
+    while (count <= 10) {
+      total += count;
+      count += 1;
+      console.log(typeof count);
+    }
+    return total;
+    }
+};
 
-}
+
+// what are the inputs to the problem?
+// An array of strings
+// the outputs?
+// a single string of the largest sum number
+// what variables should be created, and what initial variables should they have?
+//sum = 0
+// what intermediate steps should be taken to compute other values and to ultimately
+// compute the output?
+//remove dashes
+// find the sum of each index
+// compare the sums to the other numbers
+// return the highest sum or the last if multiple as a string
+// Do these steps repeat instructions that can be written in simplified form using a loop?
