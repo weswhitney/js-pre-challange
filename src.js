01b48969ccdbab965ec5414c0c5d2600d0288830
weswhitney <wesleywhitney@gmail.com>
2015-06-17 19:01:24 -0600

    finished test

--- src.js
+++ src.js
@@ -3,10 +3,10 @@ module.exports = {
   largestPhoneNumber: function(arrayOfNumbers) {
     var arrayOfSums = [], splitArray = [];
 
-      for (var j = 0; j <= arrayOfNumbers.length - 1; j++) {
-        arrayOfSums.push(arrayOfNumbers[j].replace(/[\D]/g, "").split("").map(Number).reduce(function (pv, cv) { return pv + cv; }, 0));
-      }
-        var i = arrayOfSums.lastIndexOf(Math.max.apply(Math, arrayOfSums));
-        return arrayOfNumbers[i];
+    for (var j = 0; j <= arrayOfNumbers.length - 1; j++) {
+      arrayOfSums.push(arrayOfNumbers[j].replace(/[\D]/g, "").split("").map(Number).reduce(function (pv, cv) { return pv + cv; }, 0));
     }
+    var i = arrayOfSums.lastIndexOf(Math.max.apply(Math, arrayOfSums));
+    return arrayOfNumbers[i];
+  }
 }
